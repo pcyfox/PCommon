379f944f6b9e9ff95dd4219a735e8f5e99fe7c9d
sptlt <1037050764@qq.com>
2022-09-08 14:11:12 +0800

    1、fix bug in LoadLocationDataUtils

--- lib_utils/src/main/java/com/pcommon/lib_utils/LoadLocationDataUtils.kt
+++ lib_utils/src/main/java/com/pcommon/lib_utils/LoadLocationDataUtils.kt
@@ -1,6 +1,5 @@
 package com.pcommon.lib_utils
 
-import android.util.Log
 import com.blankj.utilcode.util.GsonUtils
 import com.google.gson.Gson
 import java.io.*
@@ -19,25 +18,20 @@ object LoadLocationDataUtils {
             while (reader.readLine().also { line = it } != null) {
                 jsonString.append(line)
             }
-            val data = Gson().fromJson(jsonString.toString(), clazz)
-            if (data != null) {
-                return data
-            }
+            return Gson().fromJson(jsonString.toString(), clazz)
         } catch (e: Exception) {
             e.printStackTrace()
         } finally {
-            if (reader != null) {
-                try {
-                    reader.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
+            try {
+                reader?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
         }
         return null
     }
 
-    fun saveObjectToSD(data: Any?, path: String) {
+    fun saveObjectToSD(data: Any?, path: String): Boolean {
         val file = File(path)
         if (file.exists() && file.canRead()) {
             file.delete()
@@ -47,14 +41,14 @@ object LoadLocationDataUtils {
             fWriter = FileWriter(path, false)
             val json = GsonUtils.toJson(data)
             fWriter.write(json)
+            fWriter.flush()
+            return true
         } catch (ex: IOException) {
             ex.printStackTrace()
+            return false
         } finally {
             try {
-                if (fWriter != null) {
-                    fWriter.flush()
-                    fWriter.close()
-                }
+                fWriter?.close()
             } catch (ex: IOException) {
                 ex.printStackTrace()
             }
